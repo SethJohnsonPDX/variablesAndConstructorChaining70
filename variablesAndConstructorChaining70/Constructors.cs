082ad85cc0a29a3c64e2957948424f0a739af253
SethJohnsonPDX <sethjohnsonpdx@gmail.com>
2018-05-16 20:51:36 -0700

    commit all

---
+++ variablesAndConstructorChaining70/Constructors.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variablesAndConstructorChaining70
+{
+    class Constructors
+    {
+        public string Model { get; set; }
+        public string Type { get; set; }
+        public int WW { get; set; }
+        public Constructors(int wheelWidth) : this("Haro", "BMX", wheelWidth)
+        {
+        }
+        public Constructors(string model, string type, int wheelWidth)
+        {
+            Model = model;
+            Type = type;
+            WW = wheelWidth;
+        }
+    }
+}
--- variablesAndConstructorChaining70/Program.cs
+++ variablesAndConstructorChaining70/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace variablesAndConstructorChaining70
 {
@@ -10,6 +11,24 @@ namespace variablesAndConstructorChaining70
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter a bike model: ");
+            string model = Console.ReadLine();
+            Console.WriteLine("Enter a bike type: ");
+            string type = Console.ReadLine();
+            Console.WriteLine("Enter a wheel width: ");
+            int ww = Convert.ToInt32(Console.ReadLine());
+
+            Constructors yourBike = new Constructors(ww);
+            // This is a const and cannot change
+            const string theName = "This is the name that will not change.";
+            Console.WriteLine(theName);
+            Console.ReadLine();
+            File.WriteAllText(@"C:\Users\Student\Desktop\theName.txt", theName);
+            // This is a generic var whose type will be interpreted based on the content
+            var readBack = File.ReadAllText(@"C:\Users\Student\Desktop\theName.txt");
+            Console.WriteLine(readBack);
+            Console.WriteLine("Your bike specs are: {0}, {1}, {2}", yourBike.Model, yourBike.Type, yourBike.WW);
+            Console.ReadLine();
         }
     }
 }
